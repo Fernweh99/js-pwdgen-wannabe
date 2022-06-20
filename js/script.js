1b66714906bf3fafa29eb642dceb9f2e0ac96434
Fernweh99 <daniele.dimento@gmail.com>
2022-06-20 13:40:23 +0200

    Recover element from DOM

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Recupero l'elemento dal DOM
+const pswGen = document.getElementById("password-gen")
+console.log(pswGen)
+//Chiedo all'utente il suo nome (creo una const)
+//Chiedo all'utente il suo cognome (creo una const)
+//Chiedo all'utente il suo colore preferito (creo una const)
+//Creo una const con la somma delle 3 costanti
+//Stampo in pagina la pass Generata
+
+
+
+
