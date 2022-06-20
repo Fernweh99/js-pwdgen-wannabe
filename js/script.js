6577da0343131004ac397bb7d48cb81a06bff6fe
Fernweh99 <daniele.dimento@gmail.com>
2022-06-20 13:45:13 +0200

    Ask and save second name

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ console.log(pswGen);
 const firstName = prompt("Come ti chiami?");
 console.log(firstName);
 //Chiedo all'utente il suo cognome (creo una const)
+const secondName = prompt("Qual'è il tuo cognome?");
+console.log(secondName);
 //Chiedo all'utente il suo colore preferito (creo una const)
+
 //Creo una const con la somma delle 3 costanti
 //Stampo in pagina la pass Generata
 
