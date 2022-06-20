e42446d58f8a6b230883f1970b2c33b8508969c1
Fernweh99 <daniele.dimento@gmail.com>
2022-06-20 13:43:39 +0200

    Ask and save first name

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //Recupero l'elemento dal DOM
-const pswGen = document.getElementById("password-gen")
-console.log(pswGen)
+const pswGen = document.getElementById("password-gen");
+console.log(pswGen);
 //Chiedo all'utente il suo nome (creo una const)
+const firstName = prompt("Come ti chiami?");
+console.log(firstName);
 //Chiedo all'utente il suo cognome (creo una const)
 //Chiedo all'utente il suo colore preferito (creo una const)
 //Creo una const con la somma delle 3 costanti
